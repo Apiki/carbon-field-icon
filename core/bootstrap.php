eb134eb78956cd505be2b8068ce26bdf830b7201
Elvis Henrique Pereira <elvishp2006@gmail.com>
2020-04-24 18:01:40 -0300

    Update bootstrap.php

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -6,6 +6,10 @@ use Carbon_Field_Icon\Icon_Field;
 use Carbon_Field_Icon\Providers\Dashicons_Provider;
 use Carbon_Field_Icon\Providers\Font_Awesome_Provider;
 
+if ( ! function_exists( 'add_action' ) ) {
+	return;
+}
+
 define( 'Carbon_Field_Icon\\VERSION', '3.0.0' );
 define( 'Carbon_Field_Icon\\DIR', dirname( __DIR__ ) );
 
